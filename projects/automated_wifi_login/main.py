fb4bfb4860a485bcd26595faf907822c7f909e43
Rengeten1 <rownakdeb2@gmail.com>
2025-09-27 19:33:50 +0200

    removed print url

--- projects/automated_wifi_login/main.py
+++ projects/automated_wifi_login/main.py
@@ -7,8 +7,5 @@ dotenv.load_dotenv()
 # url_login = "http://hs1-4-amplus.spot/login?dst=&username=xxxxxxxxxxxxxxxxxxxxxxxxxxx"
 url_login = os.getenv("url")
 
-# print the url to verify
-print(url_login)
-
 # get request
 response = requests.get(url_login)
